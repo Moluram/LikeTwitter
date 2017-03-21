aa1733b452740189b9d24d1c11d8144963f1fecb
Aliaksei Chorny <moluram.a@gmail.com>
2017-03-21 21:57:41 +0300

    Minimal working app based on Spring MVC Hello World

---
+++ src/main/java/twitter/configuration/TwitterWebConfiguration.java
@@ -0,0 +1,34 @@
+package twitter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "twitter.controller")
+public class TwitterWebConfiguration extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	/**
+	 * Configure static content handling.
+	 */
+	@Override
+	public void configureDefaultServletHandling( DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
