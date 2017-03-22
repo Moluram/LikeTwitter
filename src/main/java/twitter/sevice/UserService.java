7ec7a6f189fe0e351989d7b504bdc7d0d13b4507
Aliaksei Chorny <acreed3003@gmail.com>
2017-03-22 18:06:52 +0300

    Add service interface for users

--- src/main/java/twitter/sevice/UserService.java
+++ src/main/java/twitter/sevice/UserService.java
@@ -1,28 +1,28 @@
-package twitter.dao;
+package twitter.sevice;
 
 import twitter.beans.User;
 
 import java.util.List;
 
 /**
- * Represents an interface to work with database
+ * Represents an interface to work with users in app
  * @author Aliaksei Chorny
  */
-public interface UserDAO {
+public interface UserService {
   /**
-   * Adds user to the database
+   * Adds user to the app
    * @param user - user to add
    */
   void addUser(User user);
 
   /**
-   * Returns list of User's from this database
+   * Returns list of User's from this app
    * @return List<User> - list of User's
    */
   List<User> listUser();
 
   /**
-   * Removes user from db
+   * Removes user from app
    * @param id - user id
    */
   void removeUser(Integer id);
