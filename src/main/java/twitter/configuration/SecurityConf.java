1a29131805eb72ce7fba6ecc090a4f67d78c313e
Aliaksei Chorny <acreed3003@gmail.com>
2017-03-25 15:50:39 +0300

    Add password encoder

--- src/main/java/twitter/configuration/SecurityConf.java
+++ src/main/java/twitter/configuration/SecurityConf.java
@@ -1,12 +1,16 @@
 package twitter.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import twitter.service.MyUserDetailsService;
 
 /**
  * Class configuration to a spring security
@@ -17,13 +21,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity
 public class SecurityConf
     extends WebSecurityConfigurerAdapter {
+  private MyUserDetailsService userDetailsService;
+
   @Autowired
-  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("best")
-        .password("123456").roles("USER");
-    auth.inMemoryAuthentication().withUser("admin")
-        .password("123456").roles("ADMIN");
-    auth.inMemoryAuthentication().withUser("dba")
-        .password("123456").roles("DBA");
+  public void setUserDetailsService(MyUserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
