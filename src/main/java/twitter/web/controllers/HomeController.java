0ac4009115e90480b22253596e6b494ca8a6c0d5
nborsuk <borsuk.nikolay.96@gmail.com>
2017-04-05 01:13:23 +0200

    Add create and update method to user dao

--- src/main/java/twitter/web/controllers/HomeController.java
+++ src/main/java/twitter/web/controllers/HomeController.java
@@ -1,11 +1,15 @@
 package twitter.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import twitter.beans.User;
+import twitter.dao.user.UserDAO;
+import twitter.dao.user.UserDAOJdbcImpl;
 import twitter.service.user.UserService;
 
 @Controller
@@ -13,9 +17,23 @@ import twitter.service.user.UserService;
 @RequestMapping("/")
 class HomeController {
 
+	private final UserDAO userDAO;
+
+	@Autowired
+	public HomeController(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView sayHello(ModelAndView model) {
 		model.setViewName("welcome");
+		User user=new User();
+		user.setUsername("Yla");
+		user.setPassword("234");
+		user.setEmail("sdfsd@dsfd");
+		user.setEnabled(false);
+		user.setTokenExpired(false);
+		userDAO.create(user);
 		return model;
 	}
 
