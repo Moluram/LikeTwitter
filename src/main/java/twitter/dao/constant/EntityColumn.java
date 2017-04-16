55e99f25bdc84f5352ea8c62f147b0ea953bd438
nborsuk <borsuk.nikolay.96@gmail.com>
2017-04-16 13:05:45 +0200

    Add entity constants

---
+++ src/main/java/twitter/dao/constant/EntityColumn.java
@@ -0,0 +1,17 @@
+package twitter.dao.constant;
+
+/**
+ * Created by Nikolay on 16.04.2017.
+ */
+public class EntityColumn {
+
+  public static final String COLUMN_USERNAME = "username";
+  public static final String COLUMN_PASSWORD = "password";
+  public static final String COLUMN_EMAIL = "email";
+  public static final String COLUMN_ENABLED = "enabled";
+  public static final String COLUMN_TOKEN_EXPIRED = "token_expired";
+
+  public static final String COLUMN_PASSWORD_RESET_TOKEN = "p_token";
+  public static final String COLUMN_VERIFICATION_TOKEN = "v_token";
+
+}
