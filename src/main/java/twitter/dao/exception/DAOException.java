711043ff962b6dfd16a4954db1921bb4b5816964
nborsuk <borsuk.nikolay.96@gmail.com>
2017-04-16 12:18:29 +0200

    Add dao exeption class

---
+++ src/main/java/twitter/dao/exception/DAOException.java
@@ -0,0 +1,27 @@
+package twitter.dao.exception;
+
+/**
+ * Created by Nikolay on 15.04.2017.
+ */
+public class DAOException extends Exception{
+
+  public DAOException() {
+  }
+
+  public DAOException(String message) {
+    super(message);
+  }
+
+  public DAOException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public DAOException(Throwable cause) {
+    super(cause);
+  }
+
+  public DAOException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
